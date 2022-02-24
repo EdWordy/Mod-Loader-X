d2b90d37241d51d0236ed975567dbf09c9842908
TedDraws <edwards730@hotmail.com>
2022-02-23 21:46:53 -0600

    v0.1.1
    keep on keeping on

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -17,59 +17,88 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 
-public class JavaFXApplication1 extends Application { 
+public class JavaFXApplication1 extends Application 
+{ 
+ 
+    boolean modsLoaded;
+    boolean modsZero = true;
+    int numOfModsLoaded;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+    public static void main(String[] args) 
+        {
+             launch(args);
+        }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("ModLoaderUI.fxml"));
-        primaryStage.setTitle("Mod Loader X v0.1");
-        primaryStage.setScene(new Scene(root, 900, 600));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
+    public void start(Stage primaryStage) throws Exception 
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("ModLoaderUI.fxml"));
+            primaryStage.setTitle("Mod Loader X v0.1");
+            primaryStage.setScene(new Scene(root, 900, 600));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+            
+            modPathFinder();
         }
 
     public void exitButtonClicked()
         {
-           System.exit(0);
+            System.exit(0);
         }
 
     public void helpButtonClicked()
         {
-        
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Mod Loader X v0.1 help");
             alert.setHeaderText(null);
             alert.setContentText("Mod Loader X v0.1 was written in java 8 using javafx8 and was intended for use with the game Space Haven alpha 14.1.");
-
             alert.showAndWait();
         }
 
     public void addMod()
         {
-        
-        // TO DO
-        System.out.println("..Mod Added..");
-        // some code here
+            // counter
+            numOfModsLoaded += 1;
+            System.out.println("Mods: " + numOfModsLoaded);
+            System.out.println("..Mod Added..");           
+
+           
+
         }
 
     public void removeMod()
         {
-        
-        // TO DO
-        System.out.println("..Mod Removed..");
-        // some code here
+             // counter
+            if (modsZero = false){
+            numOfModsLoaded -= 1;
+            System.out.println("..Mod Removed..");
+            System.out.println("Mods: " + numOfModsLoaded);   
+            }
+
+
         }
 
-      
+      public void modPathFinder()
+       {
+           Path rootPath = Paths.get("F:/Games/SteamLibrary/steamapps/common/SpaceHaven/mods");
+       }
 
+      public void launcher()
+       {
+
+       }
+
+       
+       
+       
 
 
 }
